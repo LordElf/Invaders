afe72b048aff8109ef349fd469d5cbd98e91cc33
XFWoan <XFWoan@163.com>
2018-10-13 05:18:44 +0800

    second commit

--- Invaders/Invaders/Form1.Designer.cs
+++ Invaders/Invaders/Form1.Designer.cs
@@ -1,6 +1,15 @@
-﻿namespace Invaders
+﻿
+namespace Invaders
 {
-    partial class Form1
+    /// <summary>
+    /// 窗体包括一些定时器来通知游戏继续进行，它要传递按键事件，并完成入侵者和闪
+    /// 烁星星的动画。它还有一个paint事件处理程序来绘制图形，这个事件处理程序只
+    /// 需调用Game对象的draw()方法
+    /// </summary>
+    /// <remarks>
+    /// 2018.10.13: 创建. by woan <br/>
+    /// </remarks>
+    partial class Form
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -25,15 +34,43 @@
         /// <summary>
         /// 设计器支持所需的方法 - 不要修改
         /// 使用代码编辑器修改此方法的内容。
+        /// woan：然而我还是修改了
         /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.animationTimer = new System.Windows.Forms.Timer(this.components);
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // animationTimer
+            // 
+            this.animationTimer.Interval = 2000;
+            this.animationTimer.Tick += new System.EventHandler(this.animationTimer_Tick);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 10;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.MaximizeBox = false;
+            this.Name = "Form";
+            this.Text = "Invaders";
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form_KeyDown);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer animationTimer;
+        private System.Windows.Forms.Timer gameTimer;
     }
 }
 
--- Invaders/Invaders/Form1.cs
+++ Invaders/Invaders/Form1.cs
@@ -10,11 +10,39 @@ using System.Windows.Forms;
 
 namespace Invaders
 {
-    public partial class Form1 : Form
+    /// <summary>
+    /// 窗体包括一些定时器来通知游戏继续进行，它要传递按键事件，并完成入侵者和闪
+    /// 烁星星的动画。它还有一个paint事件处理程序来绘制图形，这个事件处理程序只
+    /// 需调用Game对象的draw()方法
+    /// </summary>
+    /// <remarks>
+    /// 2018.10.13: 创建. by woan <br/>
+    /// </remarks>
+    public partial class Form : System.Windows.Forms.Form
     {
-        public Form1()
+        public Form()
         {
             InitializeComponent();
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            animationTimer.Start();
+        }
+
+        private void animationTimer_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form_KeyDown(object sender, KeyEventArgs e)
+        {
+
         }
     }
 }
